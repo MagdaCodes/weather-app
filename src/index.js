763fcb1a7e56bc6a7dc1495bd10e1071f3d6a852
MagdaCodes <themsway123@gmail.com>
2022-04-16 16:03:27 +0200

    Code clean up

--- src/index.js
+++ src/index.js
@@ -75,10 +75,8 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "32e21d67cb89f007d8b2ad84f78f5d7f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 function showTemperature(response) {
